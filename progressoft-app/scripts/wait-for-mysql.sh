50347f041e32be5990a6256dbc7579275cee4d02
Ahmad Dagher <Ahmad.dagher@progressoft.com>
2025-06-11 12:49:31 +0300

    update the Vars

---
+++ progressoft-app/scripts/wait-for-mysql.sh
@@ -0,0 +1,12 @@
+ #!/bin/sh
+    RETRIES=30
+    until mysql -h my-release-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "SELECT 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+      echo "Waiting for MySQL... $((RETRIES--))"
+      sleep 5
+    done
+
+    if [ $RETRIES -eq 0 ]; then
+      echo "MySQL not ready after 30 tries"
+      exit 1
+    fi
+    echo "MySQL is up"
